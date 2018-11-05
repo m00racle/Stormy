815e5a0d76f79d1042bc5ee2563c3c4670e36175
mooracle <yanuar.prakosa@gmail.com>
2018-11-05 12:25:16 +0700

    Entry 2: Making a HTTP GET Request with OkHttp (Error)

--- app/src/main/java/com/mooracle/stormy/MainActivity.java
+++ app/src/main/java/com/mooracle/stormy/MainActivity.java
@@ -2,12 +2,41 @@ package com.mooracle.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+   public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "545ef8a2ce431db1078ae75d9a13b954";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/"
+                + apiKey + "/"
+                + latitude + ","
+                + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()){
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "IO Exception caught: ", e);
+        }
     }
 }
