9811ba10a1bbe86350311ca38cd5a34c24b54ce0
mooracle <yanuar.prakosa@gmail.com>
2018-11-07 23:00:35 +0700

    Entry 6: Creating the model

---
+++ app/src/main/java/com/mooracle/stormy/CurrentWeather.java
@@ -0,0 +1,69 @@
+package com.mooracle.stormy;
+
+public class CurrentWeather {
+    //define fields variables
+    private String locationLabel;
+    private String icon;
+    private long time;
+    private double temperature;
+    private double humidity;
+    private double percipChance;
+    private String summary;
+
+    // getters and setters
+    public String getLocationLabel() {
+        return locationLabel;
+    }
+
+    public void setLocationLabel(String locationLabel) {
+        this.locationLabel = locationLabel;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getPercipChance() {
+        return percipChance;
+    }
+
+    public void setPercipChance(double percipChance) {
+        this.percipChance = percipChance;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+}
