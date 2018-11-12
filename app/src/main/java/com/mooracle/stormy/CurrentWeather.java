0f3b9e319f06ec9b010a09c8083048f27a739ca2
mooracle <yanuar.prakosa@gmail.com>
2018-11-12 20:53:59 +0700

    Entry 10: Setting The Weather Icons (No drawables uploaded)

--- app/src/main/java/com/mooracle/stormy/CurrentWeather.java
+++ app/src/main/java/com/mooracle/stormy/CurrentWeather.java
@@ -41,6 +41,45 @@ public class CurrentWeather {
         this.icon = icon;
     }
 
+    public int getIconId(){
+        int iconId;
+        switch (icon){
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.drawable.clear_night;
+                break;
+            case "rain":
+                iconId  = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+                default:
+                    iconId = R.drawable.clear_day;
+        }
+        return iconId;
+    }
+
     public long getTime() {
         return time;
     }
